9921710b6d22ae5c68ed1896291bd403e18c0c19
czlucius <58442255+czlucius@users.noreply.github.com>
2020-10-23 20:57:30 +0800

    Push project

---
+++ HelloWorldIntro/HelloWorldIntro/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace HelloWorldIntro
+{
+    public partial class MainPage : ContentPage
+    {
+        private int Count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+            ((Button)FindByName("ClickButton")).Text = "Runtime initialised text.";
+
+        }
+
+        private void ClickButton_Clicked(object sender, EventArgs e)
+        {
+            Count++;
+            ((Button)sender).Text = $"You've clicked {Count} times. ";
+        }
+
+       
+    }
+}
